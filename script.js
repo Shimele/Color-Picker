088f7ccafc0a98e5bb6e4428ea8bc3b1b5cae9e6
shimele <bechembayamba@gmail.com>
2020-02-13 13:57:45 +0100

    display fix

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ document.querySelector("#main-canvas").addEventListener("click", e =>{
     document.querySelector("#hexVal").style.backgroundColor = rgbPixelColor
     document.querySelector("#rgbVal").style.borderColor = rgbPixelColor
     document.querySelector("#hexVal").style.borderColor = rgbPixelColor
-    document.querySelectorAll(".display-color-value").style.color = rgbPixelColor
+    document.querySelectorAll("div.display-color-value").style.color = rgbPixelColor
     document.querySelector("#main-canvas").style.backgroundColor = rgbPixelColor
 
     //draw a curve in canvas area
